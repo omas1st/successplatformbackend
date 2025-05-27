c80651ae6c935171f77b5764f41a00fb2b4ed69d
omas1st <omas1st@gmail.com>
2025-05-27 23:56:57 +0100

    Update adminController.js

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,8 +1,8 @@
-const User       = require("../models/User");
-const LottoBall  = require("../models/LottoBall");
+const User = require("../models/User");
+const LottoBall = require("../models/LottoBall");
 const PastWinning = require("../models/PastWinning");
-const Result     = require("../models/Result");
-const sendMail   = require("../utils/mailer");
+const Result = require("../models/Result");
+const sendMail = require("../utils/mailer");
 
 // List all users
 exports.listUsers = (_, res) =>
@@ -23,41 +23,26 @@ exports.deleteUser = (req, res) =>
 
 // Get editable balls for home page
 exports.getBalls = async (_, res) => {
-  const [free, lunchtime, teatime] = await Promise.all([
-    LottoBall.findOne({ type: "free", date: { $gte: startOfToday() } }),
-    LottoBall.findOne({ type: "lunchtime", date: { $gte: startOfToday() } }),
-    LottoBall.findOne({ type: "teatime", date: { $gte: startOfToday() } }),
-  ]);
-
+  const free = await LottoBall.findOne({ type: "free", date: { $gte: startOfToday() } });
+  const lunchtime = await LottoBall.findOne({ type: "lunchtime", date: { $gte: startOfToday() } });
+  const teatime = await LottoBall.findOne({ type: "teatime", date: { $gte: startOfToday() } });
   res.json({
-    free: free?.balls || Array(14).fill({ value: "00", isWon: false }),
-    premium: {
-      lunchtime: lunchtime?.balls || Array(4).fill({ value: "00", isWon: false }),
-      teatime: teatime?.balls || Array(4).fill({ value: "00", isWon: false })
-    }
+    free: free.balls,
+    premium: { lunchtime: lunchtime.balls, teatime: teatime.balls }
   });
 };
 
 // Update homepage balls (admin panel)
 exports.updateBalls = async (req, res) => {
   const { free, premium } = req.body;
-  await LottoBall.updateOne(
-    { type: "free", date: { $gte: startOfToday() } },
-    { balls: free }
-  );
-  await LottoBall.updateOne(
-    { type: "lunchtime", date: { $gte: startOfToday() } },
-    { balls: premium.lunchtime }
-  );
-  await LottoBall.updateOne(
-    { type: "teatime", date: { $gte: startOfToday() } },
-    { balls: premium.teatime }
-  );
+  await LottoBall.updateOne({ type: "free", date: { $gte: startOfToday() } }, { balls: free });
+  await LottoBall.updateOne({ type: "lunchtime", date: { $gte: startOfToday() } }, { balls: premium.lunchtime });
+  await LottoBall.updateOne({ type: "teatime", date: { $gte: startOfToday() } }, { balls: premium.teatime });
   res.json({ message: "Balls updated" });
 };
 
 // Get past-winning records
-exports.getPastWinning = (_, res) =>
+exports.getPastWinning = (_, res) =>  // <-- ADDED THIS FUNCTION
   PastWinning.find().then(records => res.json(records));
 
 // Create new past-winning records
@@ -65,7 +50,7 @@ exports.updatePastWinning = async (req, res) => {
   try {
     await Promise.all(req.body.records.map(r =>
       PastWinning.create({ type: r.type, balls: r.balls, date: new Date() })
-    );
+    ));
     res.json({ message: "Past-winning updated" });
   } catch (err) {
     console.error(err);
@@ -74,7 +59,7 @@ exports.updatePastWinning = async (req, res) => {
 };
 
 // Get past-results records
-exports.getPastResults = (_, res) =>
+exports.getPastResults = (_, res) =>  // <-- ADDED THIS FUNCTION
   Result.find().then(records => res.json(records));
 
 // Create new past-results records
@@ -82,7 +67,7 @@ exports.updatePastResults = async (req, res) => {
   try {
     await Promise.all(req.body.records.map(r =>
       Result.create({ type: r.type, balls: r.balls, date: new Date() })
-    );
+    ));
     res.json({ message: "Past-results updated" });
   } catch (err) {
     console.error(err);
@@ -104,15 +89,12 @@ exports.getRedirects = (req, res) =>
 
 // Update custom URLs for a user
 exports.updateRedirects = (req, res) =>
-  User.findByIdAndUpdate(
-    req.params.id,
-    { customUrls: req.body.customUrls },
-    { new: true }
-  ).then(u => res.json(u.customUrls));
+  User.findByIdAndUpdate(req.params.id, { customUrls: req.body.customUrls }, { new: true })
+      .then(u => res.json(u.customUrls));
 
 // Utility
 function startOfToday() {
   const d = new Date();
-  d.setUTCHours(0, 0, 0, 0); // Changed to midnight UTC
+  d.setUTCHours(0, 1, 0, 0);
   return d;
 }
