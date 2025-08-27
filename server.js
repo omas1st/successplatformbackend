cd135ed419de51ae6f827ac7154a7cd2aef7e2c4
omas1st <omas1st@gmail.com>
2025-08-27 01:47:59 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 // Configure CORS for production
 app.use(cors({
   origin: [
-    "https://uk49success.vercel.app", // Your frontend URL
+    "https://successuk49.vercel.app", // Your frontend URL
     "http://localhost:3000" // Local development
   ],
   credentials: true,
