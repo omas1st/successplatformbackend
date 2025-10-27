b27e88fa7ce27335cd8f4e745fe3afb7fdbef97c
omas1st <omas1st@gmail.com>
2025-10-27 23:03:59 +0100

    Update CORS configuration for frontend URL

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 // Configure CORS for production
 app.use(cors({
   origin: [
-    "https://successuk49.vercel.app", // Your frontend URL
+    "https://successplatformbackend-eta.vercel.app/", // Your frontend URL
     "http://localhost:3000" // Local development
   ],
   credentials: true,
