92bab543e81d9a76ff839299e61d8131d065b3c3
omas1st <omas1st@gmail.com>
2025-10-28 02:17:36 +0100

    Remove unused route for free lotto

--- routes/lottoRoutes.js
+++ routes/lottoRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router  = express.Router();
 const lotto   = require('../controllers/lottoController');
 
-router.get('/free',         lotto.getFree);
 router.get('/premium',      lotto.getPremium);
 router.get('/past-winning', lotto.getPastWinning);
 router.get('/past-results', lotto.getPastResults);
